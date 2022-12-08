40edfb52c323caa9f02003ddca2c6e527e805089
Ritchie funes <43680055+rfunes100@users.noreply.github.com>
2022-12-07 23:13:46 -0600

    proyecto de examen

---
+++ examenautor/biblioteca.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace examenautor
+{
+    partial class biblioteca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvautores = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtlibro = new System.Windows.Forms.TextBox();
+            this.txtanio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbautor = new System.Windows.Forms.ComboBox();
+            this.cmbcategoria = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvautores)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvautores);
+            this.groupBox1.Location = new System.Drawing.Point(44, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(740, 193);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listado de autores";
+            // 
+            // dgvautores
+            // 
+            this.dgvautores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvautores.Location = new System.Drawing.Point(17, 21);
+            this.dgvautores.Name = "dgvautores";
+            this.dgvautores.RowHeadersWidth = 51;
+            this.dgvautores.RowTemplate.Height = 24;
+            this.dgvautores.Size = new System.Drawing.Size(723, 150);
+            this.dgvautores.TabIndex = 0;
+            this.dgvautores.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvautores_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btneditar);
+            this.groupBox2.Controls.Add(this.btnagregar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtlibro);
+            this.groupBox2.Controls.Add(this.txtanio);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cmbautor);
+            this.groupBox2.Controls.Add(this.cmbcategoria);
+            this.groupBox2.Location = new System.Drawing.Point(44, 272);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(697, 255);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "mantenimiento";
+            // 
+            // btneditar
+            // 
+            this.btneditar.Location = new System.Drawing.Point(302, 184);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(75, 23);
+            this.btneditar.TabIndex = 9;
+            this.btneditar.Text = "editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            this.btneditar.Click += new System.EventHandler(this.btneditar_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(195, 184);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.TabIndex = 8;
+            this.btnagregar.Text = "agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "year:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "libro:";
+            // 
+            // txtlibro
+            // 
+            this.txtlibro.Location = new System.Drawing.Point(459, 120);
+            this.txtlibro.Name = "txtlibro";
+            this.txtlibro.Size = new System.Drawing.Size(197, 22);
+            this.txtlibro.TabIndex = 5;
+            // 
+            // txtanio
+            // 
+            this.txtanio.Location = new System.Drawing.Point(110, 120);
+            this.txtanio.Name = "txtanio";
+            this.txtanio.Size = new System.Drawing.Size(197, 22);
+            this.txtanio.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "categoria:";
+            // 
+            // cmbautor
+            // 
+            this.cmbautor.FormattingEnabled = true;
+            this.cmbautor.Location = new System.Drawing.Point(459, 65);
+            this.cmbautor.Name = "cmbautor";
+            this.cmbautor.Size = new System.Drawing.Size(197, 24);
+            this.cmbautor.TabIndex = 1;
+            // 
+            // cmbcategoria
+            // 
+            this.cmbcategoria.FormattingEnabled = true;
+            this.cmbcategoria.Location = new System.Drawing.Point(110, 68);
+            this.cmbcategoria.Name = "cmbcategoria";
+            this.cmbcategoria.Size = new System.Drawing.Size(197, 24);
+            this.cmbcategoria.TabIndex = 0;
+            // 
+            // biblioteca
+            // 
+            this.ClientSize = new System.Drawing.Size(938, 594);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "biblioteca";
+            this.Text = "biblioteca";
+            this.Load += new System.EventHandler(this.biblioteca_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvautores)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvautores;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtlibro;
+        private System.Windows.Forms.TextBox txtanio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbautor;
+        private System.Windows.Forms.ComboBox cmbcategoria;
+    }
+}
+
---
+++ examenautor/biblioteca.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examenautor
+{
+    public partial class biblioteca : Form
+    {
+
+        public string conexiondbsql = Properties.Settings.Default.conexion;
+        public string sqlconsulta = "";
+        public int id = 0;
+
+        public biblioteca()
+        {
+            InitializeComponent();
+        }
+
+        private void refrescar()
+        {
+            var consulta = " select numero , cat.categoria, anio, aut.Autor, libro  from bibliotecadigital bib  " +
+                " inner join categoria  cat on cat.idcategoria = bib.Categoria "+
+                "inner join Autor$ aut on aut.IdAutor = bib.Autor";
+
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conexiondbsql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dgvautores.DataSource = ds.Tables[0];
+
+        }
+
+        private void cargarautores()
+        {
+
+            var consulta = string.Format(" select idautor, Autor from [dbo].[Autor$] " );
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conexiondbsql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmbautor.DisplayMember = "Autor";
+            cmbautor.ValueMember = "idautor";
+            cmbautor.DataSource = ds.Tables[0];
+
+
+        }
+
+        private void cargaracategoria()
+        {
+
+            var consulta = string.Format(" select idcategoria, categoria  from categoria ");
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conexiondbsql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmbcategoria.DisplayMember = "categoria";
+            cmbcategoria.ValueMember = "idcategoria";
+            cmbcategoria.DataSource = ds.Tables[0];
+
+
+        }
+
+
+
+
+        private void biblioteca_Load(object sender, EventArgs e)
+        {
+            this.refrescar();
+            this.cargaracategoria();
+            this.cargarautores();
+        }
+
+        //private void btnagregar_MouseClick(object sender, MouseEventArgs e)
+        //{
+
+        //}
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+
+            if (txtlibro.Text == "" || txtanio.Text == "")
+            {
+                MessageBox.Show("El biblioteca  esta vacio.");
+            }
+            else
+            {
+
+
+             
+
+                SqlConnection conexion = new SqlConnection(conexiondbsql);
+                conexion.Open();
+               
+                SqlCommand cmd = new SqlCommand("insertabiblioteca", conexion);
+                cmd.CommandType  =CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idcategoria", SqlDbType.Int).Value = cmbcategoria.SelectedValue;
+                cmd.Parameters.AddWithValue("@anio", SqlDbType.Int).Value = Convert.ToInt32( txtanio.Text);
+                cmd.Parameters.AddWithValue("@autor", SqlDbType.Int).Value = cmbautor.SelectedValue;
+                cmd.Parameters.AddWithValue("@Libro", SqlDbType.NVarChar).Value = txtlibro.Text;
+                cmd.ExecuteNonQuery();
+
+                txtlibro.Text = "";
+                txtanio.Text = "";
+                this.refrescar();
+                MessageBox.Show("Se agrego a la biblioteca de forma exitosa.");
+                conexion.Close();
+            }
+
+        }
+
+        private void btneditar_Click(object sender, EventArgs e)
+        {
+
+            if (txtlibro.Text == "" || txtanio.Text == "")
+            {
+                MessageBox.Show("El biblioteca  esta vacio.");
+            }
+            else
+            {
+
+
+
+
+                SqlConnection conexion = new SqlConnection(conexiondbsql);
+                conexion.Open();
+
+                SqlCommand cmd = new SqlCommand("editabiblioteca", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idcategoria", SqlDbType.Int).Value = cmbcategoria.SelectedValue;
+                cmd.Parameters.AddWithValue("@anio", SqlDbType.Int).Value = Convert.ToInt32(txtanio.Text);
+                cmd.Parameters.AddWithValue("@autor", SqlDbType.Int).Value = cmbautor.SelectedValue;
+                cmd.Parameters.AddWithValue("@Libro", SqlDbType.NVarChar).Value = txtlibro.Text;
+                cmd.Parameters.AddWithValue("@id", SqlDbType.Int).Value = id;
+
+                cmd.ExecuteNonQuery();
+
+                txtlibro.Text = "";
+                txtanio.Text = "";
+                this.refrescar();
+                MessageBox.Show("Se agrego a la biblioteca de forma exitosa.");
+                conexion.Close();
+            }
+
+        }
+
+        private void dgvautores_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            DataGridViewRow selectedrow = dgvautores.Rows[index];
+            id = Convert.ToInt32(selectedrow.Cells[0].Value);
+
+
+        }
+    }
+}
